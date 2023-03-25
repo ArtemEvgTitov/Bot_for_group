5e61f7d7a3d43fc898e33172f512da8b6a93b178
ArtemEvgTitov <artem.titov.wg@gmail.com>
2023-03-25 12:37:17 +0300

    добавлен Message Handler

--- BOT/BOT.py
+++ BOT/BOT.py
@@ -1,7 +1,7 @@
 import logger as log
 from settings import TOKEN
+import random
 import json
-import operator
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
 from telegram.ext import (
     Updater,
@@ -10,6 +10,34 @@ from telegram.ext import (
     Filters,
     ConversationHandler,
 )
+hello = []
+hello_text = []
 
-SURNAME, NAME, TEL_NUMBER = range(3)
 
+def message(update, _):
+    text = update.message.text
+    load_hello()
+    if text == 'Бот график':
+        update.message.reply_text(
+            'https://t.me/c/1159615196/69')
+    elif text in hello_text:
+        random_photo = random.choice(hello)
+        update.message.reply_text(random_photo)
+
+def load_hello():
+    global hello
+    global hello_text
+    with open("hello.json", "r", encoding="utf-8") as fh:
+        hello = json.load(fh)
+    with open("hello_text.json", "r", encoding="utf-8") as fh:
+        hello_text = json.load(fh)
+
+if __name__ == '__main__':
+    updater = Updater(token=TOKEN)
+    dispatcher = updater.dispatcher
+    message_handler = MessageHandler(Filters.text, message)
+
+dispatcher.add_handler(message_handler)
+
+updater.start_polling()
+updater.idle()
