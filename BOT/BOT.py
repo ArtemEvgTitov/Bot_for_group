2c003fc1afb30bf4047cc042531309321f9e549e
ArtemEvgTitov <artem.titov.wg@gmail.com>
2023-03-28 19:36:33 +0300

    добавлены методы вывода и сохранения графика

--- BOT/BOT.py
+++ BOT/BOT.py
@@ -12,25 +12,35 @@ from telegram.ext import (
 )
 hello = []
 hello_text = []
+work_schedule = []
 
 
 def message(update, _):
     text = update.message.text
-    load_hello()
+    load()
     if text == 'Бот график':
-        update.message.reply_text(
-            'https://t.me/c/1159615196/69')
+        update.message.reply_text(work_schedule[0])
+    elif text == '#график':
+        work_schedule.append(text)
+        save()
     elif text in hello_text:
         random_photo = random.choice(hello)
         update.message.reply_text(random_photo)
 
-def load_hello():
+def load():
     global hello
     global hello_text
+    global work_schedule
     with open("hello.json", "r", encoding="utf-8") as fh:
         hello = json.load(fh)
     with open("hello_text.json", "r", encoding="utf-8") as fh:
         hello_text = json.load(fh)
+    with open("work_schedule.json", "r", encoding="utf-8") as fh:
+        work_schedule = json.load(fh)
+
+def save():
+    with open("work_schedule.json", "w", encoding="utf-8") as graf:
+        graf.write(json.dumps(work_schedule, ensure_ascii=False))
 
 if __name__ == '__main__':
     updater = Updater(token=TOKEN)
