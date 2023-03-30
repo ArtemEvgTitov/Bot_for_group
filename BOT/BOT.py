ec352502d19ba50dae4d7dd21ca8a8e6b0c5c1ff
ArtemEvgTitov <artem.titov.wg@gmail.com>
2023-03-30 22:49:43 +0300

    корректировки на telebot

--- BOT/BOT.py
+++ BOT/BOT.py
@@ -2,12 +2,10 @@ from settings import TOKEN
 import telebot
 import random
 import json
-import re
-from telebot import types
 
 bot = telebot.TeleBot(TOKEN)
 
-films = []
+work_schedule = []
 hello = []
 hello_text = []
 
@@ -19,18 +17,31 @@ def load_hello():
     with open("hello_text.json", "r", encoding="utf-8") as fh:
         hello_text = json.load(fh)
 
+def load_graf():
+    global work_schedule
+    try:
+        with open("work_schedule.json", "r", encoding="utf-8") as graf:
+            work_schedule = json.load(graf)
+    except:
+        print("Графика нет")
+
 def is_part_in_list(str_, words):
     for word in words:
         if word in str_:
             return True
     return False
-@bot.message_handler(content_types=['text'])
+@bot.message_handler(content_types=['text', 'photo'])
 def get_text_messages(message):
     load_hello()
+    load_graf()
     if is_part_in_list(message.text, hello_text):
         random_photo = random.choice(hello)
         bot.send_photo(
             message.chat.id, photo=f'{random_photo}')
+    elif message.text == 'Бот график':
+        graf = work_schedule[0]
+        bot.send_photo(
+            message.chat.id, photo=f'{graf}')
     else:
         bot.send_message(message.from_user.id,
                          "Я тебя не понимаю. 😔")
