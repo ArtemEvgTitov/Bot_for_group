6a80a9f6b471d5f04b73c4a5d4d5720225706c1c
ArtemEvgTitov <artem.titov.wg@gmail.com>
2023-04-05 12:57:18 +0300

    Добавлено сохранение заявлений

--- BOT/BOT.py
+++ BOT/BOT.py
@@ -1,3 +1,5 @@
+import re
+from datetime import datetime
 from settings import TOKEN
 import telebot
 import random
@@ -5,10 +7,20 @@ import json
 
 bot = telebot.TeleBot(TOKEN)
 
-work_schedule = []
 hello = []
 hello_text = []
+Statements = []
 
+
+def load_statements():
+    global Statements
+    with open("Statements.json", "r", encoding="utf-8") as stat:
+        Statements = json.load(stat)
+
+def save_statements():
+    with open("Statements.json", "w", encoding="utf-8") as stat:
+        stat.write(json.dumps(Statements, ensure_ascii=False))
+    print("Заявление сохранено")
 
 def load_hello():
     global hello
@@ -19,15 +31,6 @@ def load_hello():
         hello_text = json.load(fh)
 
 
-def load_graf():
-    global work_schedule
-    try:
-        with open("work_schedule.json", "r", encoding="utf-8") as graf:
-            work_schedule = json.load(graf)
-    except:
-        print("Графика нет")
-
-
 def is_part_in_list(str_, words):
     for word in words:
         if word in str_:
@@ -35,18 +38,25 @@ def is_part_in_list(str_, words):
     return False
 
 
-@bot.message_handler(content_types=['text', 'photo'])
+@bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     load_hello()
-    load_graf()
     if is_part_in_list(message.text, hello_text):
         random_photo = random.choice(hello)
         bot.send_photo(
             message.chat.id, photo=f'{random_photo}')
-    elif message.text == 'Бот график':
-        graf = work_schedule[0]
-        bot.send_photo(
-            message.chat.id, photo=f'{graf}')
+    elif "Заявление " in message.text:
+        message.text = re.sub('Заявление ', '', message.text)
+        time = datetime.utcfromtimestamp(message.date).strftime('%Y-%m-%d')
+        statements_element = {"type": 'Заявление', "date": time, "statement": message.text}
+        Statements.append(statements_element)
+        save_statements()
+        print(statements_element)
+        bot.send_message(message.from_user.id,
+                         f"Сохранил.\nТип: {statements_element['type']}"
+                         f"\nДата: {statements_element['date']}"
+                         f"\nТекст: {statements_element['statement']}")
+
     else:
         bot.send_message(message.from_user.id,
                          "Я тебя не понимаю. 😔")
